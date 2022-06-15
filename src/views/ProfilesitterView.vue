f7f97b049797ce2bcdfefe7b075f04d3609a0737
dounya-m <mellouki.dounya@gmail.com>
2022-06-15 20:17:54 +0100

    update

--- src/views/ProfilesitterView.vue
+++ src/views/ProfilesitterView.vue
@@ -1,10 +1,34 @@
 <template>
-    <div>
-        <NavBare/>
-    </div>
 
-    <div v-for="a in this.sitter" :key="a.id_sitter">{{this.sitter.nom}}</div>
+
+    <div v-for="a in this.sitter" :key="a.id_sitter">
+
+    <section class="pb-32 bg-emerald-50 heder_sitter">
+        <div>
+            <NavBare/>
+        </div>
+        <div class="flex flex-col items-center justify-center">
+        <img class="object-cover w-56 h-56 my-3 rounded-full" v-bind:src="'../sittersProfile/' + a.ps_profile ">
+        <div>
+            <p class="font-semibold">{{a.nom}} {{a.prenom}}</p>
+            <p class="font-semibold text-gray-500">{{a.email}}</p>
+        </div>
+        </div>
+    </section>
+
+    </div>
 </template>
+
+<style scoped>
+.heder_sitter{
+    background: url("../assets/images/trace.png"), rgba(236, 255, 243, 0.778);
+    background-size: cover ;
+    background-position: center;
+    border-bottom-left-radius: 10rem;
+    border-bottom-right-radius: 10rem;
+}
+
+</style>
 
 <script>
 import axios from 'axios'
